e819c8d92b7753bed7234081683430acf3fe9f68
syairaarif <putrisyafira4444@gmail.com>
2025-10-21 14:16:43 +0700

    inisialisasi project express

---
+++ index.js
@@ -0,0 +1,7 @@
+const express = require('express');
+let mysql = require('mysql2');
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
